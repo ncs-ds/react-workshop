4385862e03943cb038859e098da6025e0f27ea53
Nikos Savvidis <nikolaos.savvidis@datasite.com>
2021-11-26 13:12:19 +0000

    React workshop day 2: parent-child interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Button } from '@material-ui/core';
+import { useState } from 'react';
 import './App.css';
+import ColourNarrative from './ColourNarrative';
 
 function App() {
+  const [myColour, setMyColour] = useState('red')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button variant="outlined" onClick={() => setMyColour('red')}>Red</Button>
+      <Button variant="contained" onClick={() => setMyColour('blue')}>Blue</Button>
+
+      <div className={`myBox ${myColour}`}></div>
+
+      <ColourNarrative colour={myColour}></ColourNarrative>
+    </>
   );
 }
 
---
+++ src/ColourNarrative.tsx
@@ -0,0 +1,18 @@
+import { Typography } from "@material-ui/core";
+import { FC } from "react";
+
+export interface ColourNarrativeProps {
+    colour: string;
+} 
+
+const ColourNarrative: FC<ColourNarrativeProps> = ({colour}) => {
+    return (
+        <>
+            <Typography variant="h6">
+                The colour is {colour}!
+            </Typography>
+        </>
+    );
+}
+
+export default ColourNarrative;
