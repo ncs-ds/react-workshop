824eda64bd356352b086aad06ae49472ebb57f53
Nikos Savvidis <nikolaos.savvidis@datasite.com>
2021-11-26 13:49:32 +0000

    formatting everything with prettier formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { Button } from '@material-ui/core';
-import { useState } from 'react';
-import './App.css';
-import ColourNarrative from './ColourNarrative';
+import { Button } from "@material-ui/core";
+import { useState } from "react";
+import "./App.css";
+import ColourNarrative from "./ColourNarrative";
 
 function App() {
-  const [myColour, setMyColour] = useState('red')
+  const [myColour, setMyColour] = useState("red");
   return (
     <>
-      <Button variant="outlined" onClick={() => setMyColour('red')}>Red</Button>
-      <Button variant="contained" onClick={() => setMyColour('blue')}>Blue</Button>
-
+      {" "}
+      <Button variant="outlined" onClick={() => setMyColour("red")}>
+        Red
+      </Button>
+      <Button variant="contained" onClick={() => setMyColour("blue")}>
+        Blue
+      </Button>
       <div className={`myBox ${myColour}`}></div>
-
       <ColourNarrative colour={myColour}></ColourNarrative>
     </>
   );
